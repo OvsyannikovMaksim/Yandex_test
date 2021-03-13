1bfb8ae95d57833fe1a7694aebe44e0ac17b383d
Ovsyannikov_M <maksim.ov@mail.ru>
2021-03-13 03:14:51 +0300

    add retrofit and pojo class, prepare fragment for adapter, change some ui

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.myapplication.ui.StocksFragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tabLayout = findViewById(R.id.tab_layout)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                //выбор
+                Log.d("TAG", "sel "+tab)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                //повторное нажатие на выбранный уже
+                Log.d("TAG", "resel "+tab)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                //показывыает какой был выбран до переключения
+                Log.d("TAG", "unsel "+tab)
+            }
+        })
+
+
+        if (savedInstanceState == null) {
+            fragmentTransaction
+                .add(R.id.container, StocksFragment())
+                .commit()
+        }
     }
+
+
+
 }
