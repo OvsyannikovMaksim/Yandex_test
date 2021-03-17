42cd478e0597ba3ed7db2aaa3d4012a998da00d1
Ovsyannikov_M <maksim.ov@mail.ru>
2021-03-17 03:03:38 +0300

    almost work, some problems with adapter

--- app/src/main/java/com/example/myapplication/CompanyAdapter.kt
+++ app/src/main/java/com/example/myapplication/CompanyAdapter.kt
@@ -1,36 +1,79 @@
 package com.example.myapplication
 
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.myapplication.databinding.StockItemBinding
 
+//private var mListener: IListener,
+class CompanyAdapter(var mData: List<CompanyInfoDst>) :
+        ListAdapter<CompanyInfoDst, CompanyAdapter.CompanyVH>(DiffCallback){
 
-val diffCallback = object : DiffUtil.ItemCallback<CompanyInfoDst>() {
-    override fun areItemsTheSame(oldItem: CompanyInfoDst, newItem: CompanyInfoDst): Boolean {
-        TODO("Not yet implemented")
-    }
+    private lateinit var binding: StockItemBinding
 
-    override fun areContentsTheSame(oldItem: CompanyInfoDst, newItem: CompanyInfoDst): Boolean {
-        TODO("Not yet implemented")
-    }
-}
-
-
-class CompanyAdapter : ListAdapter<CompanyInfoDst, CompanyVH>(diffCallback){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CompanyVH {
-        TODO("Not yet implemented")
+
+        val inflater : LayoutInflater = LayoutInflater.from(parent.context)
+        binding = StockItemBinding.inflate(inflater,parent, false)
+        return CompanyVH(binding)
+
     }
 
     override fun onBindViewHolder(holder: CompanyVH, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(mData[position])
     }
 
-}
-
-class CompanyVH (itemView: View): RecyclerView.ViewHolder(itemView){
 
 
+
+    class CompanyVH (private var itemBinding: StockItemBinding):
+        RecyclerView.ViewHolder(itemBinding.root){
+
+        fun bind(companyInfo: CompanyInfoDst){
+            Glide.with(itemView.context).load(Uri.parse(companyInfo.logo)).into(itemBinding.companyPic)
+            itemBinding.companyName.text=companyInfo.name
+            itemBinding.companyPrice.text=companyInfo.curPrice.toString()
+            itemBinding.companyTicker.text=companyInfo.ticker
+            itemBinding.companyPriceChange.text=companyInfo.priceChange.toString()
+            itemBinding.companyPriceChangePercent.text=companyInfo.priceChangePercent.toString()
+            if(companyInfo.priceChange>0.0){
+
+                itemBinding.companyPriceChange.setTextColor(ContextCompat.getColor(itemView.context, R.color.green))
+                itemBinding.companyPriceChangePercent.setTextColor(ContextCompat.getColor(itemView.context, R.color.green))
+
+            }
+            else{
+
+                itemBinding.companyPriceChange.setTextColor(ContextCompat.getColor(itemView.context, R.color.red))
+                itemBinding.companyPriceChangePercent.setTextColor(ContextCompat.getColor(itemView.context, R.color.red))
+
+            }
+        }
+    }
+
+
+
+    object DiffCallback : DiffUtil.ItemCallback<CompanyInfoDst>(){
+        override fun areItemsTheSame(oldItem: CompanyInfoDst, newItem: CompanyInfoDst): Boolean {
+            if(oldItem.name == newItem.name && oldItem.ticker == newItem.ticker){
+                return true
+            }
+            return false
+        }
+
+        override fun areContentsTheSame(oldItem: CompanyInfoDst, newItem: CompanyInfoDst): Boolean {
+            if(oldItem.curPrice==newItem.curPrice && oldItem.priceChange==newItem.priceChange &&
+                oldItem.priceChangePercent==newItem.priceChangePercent ){
+                return true
+            }
+            return false
+        }
+
+    }
 }
