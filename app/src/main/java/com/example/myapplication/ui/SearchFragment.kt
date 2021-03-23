63fecfcb6a29091852ee12fe8a44dd7be7d26578
Ovsyannikov_M <maksim.ov@mail.ru>
2021-03-23 23:10:00 +0300

    add search fragment

---
+++ app/src/main/java/com/example/myapplication/ui/SearchFragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentSearchBinding
+
+class SearchFragment: Fragment() {
+
+    private var binding: FragmentSearchBinding?=null
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding= FragmentSearchBinding.inflate(inflater)
+
+        return binding!!.root
+    }
+}
